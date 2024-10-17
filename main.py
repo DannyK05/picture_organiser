7299b6997901194fb794c2ee33ca54bf4fe7ef39
DannyK05 <olubanjok@gmail.com>
2024-10-17 22:36:05 +0100

    fix: fixed the extention check issue

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ import shutil
 
 downloads_folder = os.path.expanduser("~/Downloads")
 images_folder = os.path.join(downloads_folder, "images")
-extentions = ["jpg", "jpeg", "avif", "png", "tiff"]
+extensions = [".jpg", ".jpeg", ".avif", ".png", ".tiff"]
 
 if not os.path.exists(images_folder):
     os.makedirs(images_folder)
 
 for filename in os.listdir(downloads_folder):
     file_ext = os.path.splitext(filename)[1].lower()
-    if file_ext in extentions :
-        file_path = os.join(downloads_folder, filename)
+   
+    if file_ext in extensions :
+        print(f"Moving {filename} to images folder")
+        file_path = os.path.join(downloads_folder, filename)
         shutil.move(file_path, images_folder)
-    print("Images have been organised")
+        
+    else:
+        print(f"Skipping {filename}")
+print("Images moved successfully")
